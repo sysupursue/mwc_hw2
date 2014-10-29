c3a7cacc5e5871f4e635789400fc4bd4898b2f9d
sysupursue <348556356@qq.com>
2014-10-30 00:33:39 +0800

    Use Array sort function to achieve table sort , but i still have some problem on style change.

--- group2/14214168/sorter.js
+++ group2/14214168/sorter.js
@@ -2,86 +2,110 @@
  *Date:2014.10.27
  *Author:Panbin
  *********************************************************/
-//设置点击事件的处理
-window.onload = function () {
+ window.onload = function() {
     var tables = getAllTables();
     makeAllTablesSortable(tables);
 }
 //获取表格所有数据
-function getAllTables(){
+function getAllTables() {
     return document.getElementsByTagName('table');
 }
 
 //表格排序
-function makeAllTablesSortable(tables){
-    for(var i = 0 ; i < tables.length ; i++)
-    {
+function makeAllTablesSortable(tables) {
+    for (var i = 0; i < tables.length; i++) {
         tableProcess(tables[i]);
     }
 }
 
 //单个表格处理
-function tableProcess(table)
-{
+function tableProcess(table) {
     window.eventDeal(table);
+    setBackground(table);
+}
+
+//背景设置
+function setBackground(table){
+    for(var i = 1 ; i < table.rows.length ; i++)
+    {
+        if(i%2 == 0 )
+            table.rows[i].style.backgroundColor = "lightgray";
+        else
+          table.rows[i].style.backgroundColor = "white";
+  }
 }
 
 //获取数据
-function getData(table)
-{
-        var rowArray = [ ];             //二维形式存储表格数据
-        var cellArray = [ ];
-        var rLength = table.rows.length;
-
-        for(var r = 1 ; r < rLength ; r++)
-            for(var c = 0 ; c < table.rows[r].cells.length ; c++)
-                cellArray.push(table.rows[r].cells[c].innerText);
-            rowArray.push(cellArray);
-            return rowArray;
-        }
+function getData(table) {
+    var rowArray = []; //二维形式存储表格数据
+    var cellArray;
+    var rLength = table.rows.length;
+    for (var r = 1; r < rLength; r++) {
+        cellArray = [];
+        for (var c = 0; c < table.rows[r].cells.length; c++)
+            cellArray.push(table.rows[r].cells[c].innerText);
+        rowArray.push(cellArray);
+    }
+    return rowArray;
+}
 
 //事件处理
-function eventDeal(table)
-{
-    var tableData = getData(table);                                 //用于存储单个表格数据
+function eventDeal(table) {
+    var tableData = getData(table); //用于存储单个表格数据
     var tableHead = table.rows[0].cells;
-
-    for(var i = 0 ; i < tableHead.length ; i++)
-    {
-        var sortType = " ";
-        tableHead[i].onclick = function(event)
-        {
-            //样式改变
+    for (var i = 0; i < tableHead.length; i++) {
+        var sortType ;
+        tableHead[i].onclick = function(event) {
             var clickTarget = event.target;
-            console.log("tablehead "+(clickTarget.cellIndex+1)+" has been clicked!");
-            if(sortType==" ")
-                sortType = "asc";
-            else if(sortType=="asc")
-                sortType="desc";
+            if (sortType == "asc")
+                sortType = "desc";
             else
-                sortType="asc"
-
+                sortType = "asc"
+            //样式改变
+            styleChange(table,clickTarget.cellIndex, sortType);
             //排序
-            var sorted = colSort(tableData,clickTarget.cellIndex,sortType);
-            console.log("排序后数组："+sorted.length)
+            var sorted = colSort(tableData, clickTarget.cellIndex, sortType);
             //数据更新
+            writeToTable(table, sorted);
+        }
+    }
+}
+
+//样式变更
+function styleChange(table,cellIndex,sortType){
+    if(sortType == "asc")
+    {
+        table.rows[0].cells[cellIndex].style.backgroundImage ="url('descend.png')" ;
+        table.rows[0].cells[cellIndex].style.backgroundRepeat = "no-repeat";
+        table.rows[0].cells[cellIndex].style.backgroundPosition = "right";
+    }
+    else{
+        table.rows[0].cells[cellIndex].style.backgroundImage = "url('ascend.png')";
+        table.rows[0].cells[cellIndex].style.backgroundRepeat = "no-repeat";
+    }
+}
+
+//数据更新
+function writeToTable(table, arr) {
+    var rows = table.rows;
+    for (var i = 1; i < rows.length; i++) {
+        var cells = rows[i].cells;
+        for (var j = 0; j < cells.length; j++) {
+            cells[j].innerText = arr[i - 1][j];
         }
     }
 }
 
 //按列排序
-function colSort(array,celIndex,sorttype)
-{
-    if(sorttype == " "){
-        console.log("默认");
-        return array.sort(function(x,y){ return x[celIndex]-y[celIndex];});
-    }
-    else if(sorttype == "asc"){
-        console.log("升序");
-        return array.sort(function(x,y){ return x[celIndex]-y[celIndex];});
-    }
-    else{
-        console.log("降序");
-        return array.sort(function(x,y){ return y[celIndex]-x[celIndex];});
-    }
+function colSort(array, celIndex, sorttype) {
+ if (sorttype == "asc") {
+    return array.sort(function(x, y) {
+        return x[celIndex].localeCompare(y[celIndex]);
+    });
+}
+else {
+    return array.sort(function(x, y) {
+        return y[celIndex].localeCompare(x[celIndex]);
+    });
+}
 }
